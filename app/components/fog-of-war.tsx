a7dafe2706bf16c4ff2285a4925a2159023c07db
mmmykhailo <mmmykhailo@proton.me>
2025-06-14 10:53:25 +0300

    fix fog between points far apart

--- app/components/fog-of-war.tsx
+++ app/components/fog-of-war.tsx
@@ -19,7 +19,6 @@ export const FogOfWar = ({ tracks }: { tracks: Array<Track> }) => {
       const bounds = map.getBounds();
       const topLeft = map.latLngToLayerPoint(bounds.getNorthWest());
       const bottomRight = map.latLngToLayerPoint(bounds.getSouthEast());
-
       const width = bottomRight.x - topLeft.x;
       const height = bottomRight.y - topLeft.y;
 
@@ -28,9 +27,9 @@ export const FogOfWar = ({ tracks }: { tracks: Array<Track> }) => {
       const fogWidth = width + 2 * buffer;
       const fogHeight = height + 2 * buffer;
 
-      container
-        .append("mask")
-        .attr("id", "fog-mask")
+      const mask = container.append("mask").attr("id", "fog-mask");
+
+      mask
         .append("rect")
         .attr("x", fogX)
         .attr("y", fogY)
@@ -39,20 +38,37 @@ export const FogOfWar = ({ tracks }: { tracks: Array<Track> }) => {
         .attr("fill", "white");
 
       tracks.forEach((track) => {
+        if (track.points.length === 0) return;
+
+        const radiusMeters = 100;
+        const firstPoint = track.points[0];
+        const latlng = new L.LatLng(firstPoint[0], firstPoint[1]);
+        const offsetLatLng = L.latLng(firstPoint[0] + 0.001, firstPoint[1]);
+        const radiusPixels =
+          map
+            .latLngToLayerPoint(latlng)
+            .distanceTo(map.latLngToLayerPoint(offsetLatLng)) *
+          (radiusMeters / latlng.distanceTo(offsetLatLng));
+
+        const pathData = track.points
+          .map(([lat, lng], index) => {
+            const point = map.latLngToLayerPoint(new L.LatLng(lat, lng));
+            return `${index === 0 ? "M" : "L"} ${point.x} ${point.y}`;
+          })
+          .join(" ");
+
+        mask
+          .append("path")
+          .attr("d", pathData)
+          .attr("stroke", "black")
+          .attr("stroke-width", radiusPixels * 2)
+          .attr("stroke-linecap", "round")
+          .attr("stroke-linejoin", "round")
+          .attr("fill", "none");
+
         track.points.forEach(([lat, lng]) => {
-          const latlng = new L.LatLng(lat, lng);
-          const point = map.latLngToLayerPoint(latlng);
-
-          const radiusMeters = 100;
-          const offsetLatLng = L.latLng(lat + 0.001, lng);
-          const radiusPixels =
-            map
-              .latLngToLayerPoint(latlng)
-              .distanceTo(map.latLngToLayerPoint(offsetLatLng)) *
-            (radiusMeters / latlng.distanceTo(offsetLatLng));
-
-          container
-            .select("mask")
+          const point = map.latLngToLayerPoint(new L.LatLng(lat, lng));
+          mask
             .append("circle")
             .attr("cx", point.x)
             .attr("cy", point.y)
@@ -73,7 +89,6 @@ export const FogOfWar = ({ tracks }: { tracks: Array<Track> }) => {
     };
 
     renderFog();
-
     map.on("zoomend moveend", renderFog);
 
     return () => {
