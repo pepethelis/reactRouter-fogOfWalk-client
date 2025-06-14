8242cffafae3dfd3951606e26e51181b8defd090
mmmykhailo <mmmykhailo@proton.me>
2025-06-14 10:53:41 +0300

    recenter map when adding tracks

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,4 +1,5 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 import type { Track } from "~/types";
 import { FogOfWar } from "./fog-of-war";
@@ -8,12 +9,53 @@ export type MapProps = {
   tracks: Array<Track>;
 };
 
+const MapUpdater = ({ tracks }: { tracks: Array<Track> }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (tracks.length === 0) return;
+
+    const bounds: [number, number][] = [];
+    tracks.forEach((track) => {
+      track.points.forEach((point) => {
+        bounds.push([point[0], point[1]]);
+      });
+    });
+
+    if (bounds.length > 0) {
+      map.fitBounds(bounds, { padding: [20, 20] });
+    }
+  }, [tracks, map]);
+
+  return null;
+};
+
 const Map = ({ tracks }: MapProps) => {
-  const position = { lat: 50.45, lng: 30.5233 };
+  const calculateCenter = (tracks: Array<Track>) => {
+    let totalLat = 0;
+    let totalLng = 0;
+    let pointCount = 0;
+
+    tracks.forEach((track) => {
+      track.points.forEach((point) => {
+        totalLat += point[0];
+        totalLng += point[1];
+        pointCount++;
+      });
+    });
+
+    if (pointCount === 0) {
+      return { lat: 50.45, lng: 30.5233 }; // Default position (Kyiv)
+    }
+
+    return { lat: totalLat / pointCount, lng: totalLng / pointCount };
+  };
+
+  const position = calculateCenter(tracks);
 
   return (
     <MapContainer
-      center={position}
+      center={[position.lat, position.lng]}
       zoom={13}
       className="relative h-screen w-full z-0"
     >
@@ -21,6 +63,8 @@ const Map = ({ tracks }: MapProps) => {
         attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
       />
+
+      <MapUpdater tracks={tracks} />
 
       {tracks.map((track, trackIndex) => (
         <DynamicPolyline key={trackIndex} track={track} index={trackIndex} />
