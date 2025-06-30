09d0c96329024fcae02a1e9f5646faae87a0fbb8
mmmykhailo <mmmykhailo@proton.me>
2025-06-30 13:52:45 +0300

    use voyager map style

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -109,7 +109,7 @@ const Map = ({
     >
       <TileLayer
         attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
       />
 
       <ViewportTracker onViewportChange={handleViewportChange} />
